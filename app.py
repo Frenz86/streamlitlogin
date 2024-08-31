c5ac548d7c6dee92890fac3dbf907c587749e355
Frenz86 <frenzy86@gmail.com>
2024-08-31 16:16:28 +0200

    new

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+def main():
+
+    st.title('Amazing User Login App')
+
+    ################################################
+    ## SIMPLE LOGIN
+    ################################################
+    # Create user_state
+    if 'user_state' not in st.session_state:
+        st.session_state.user_state = {
+            'username': '',
+            'password': '',
+            'logged_in': False
+        }
+
+    if not st.session_state.user_state['logged_in']:
+        # Create login form
+        st.write('Please login')
+        username = st.text_input('Username')
+        password = st.text_input('Password', type='password')
+        submit = st.button('Login')
+
+        # Check if user is logged in
+        if submit and st.session_state.user_state['logged_in'] == False:
+            if username == 'regione' and password == 'amartya':
+                st.session_state.user_state['username'] = username
+                st.session_state.user_state['password'] = password
+                st.session_state.user_state['logged_in'] = True
+                st.write('You are logged in')
+                st.rerun()
+            else:
+                st.write('Invalid username or password')
+    elif st.session_state.user_state['logged_in']:
+        #st.write('Welcome to the app')
+        st.write('You are logged in as:', st.session_state.user_state['username'])
+
+
+    ##################################################################################
+
+if __name__=="__main__":
+    main()
